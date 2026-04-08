59c0cea4ee5f986367ecd02d8cc017091656e527
Alex-Hamblin <alexmachamblin@gmail.com>
2026-04-08 02:01:48 -0400

    Updated unity scene

--- Assets/Buttons.cs
+++ Assets/Buttons.cs
@@ -1,11 +1,17 @@
+using System.Collections;
 using UnityEngine;
 
 public class Buttons : MonoBehaviour
 {
     [SerializeField] GameObject mouse;
-    
+    [SerializeField] GameObject center;
 
-    
+    [SerializeField] GameObject Light;
+    [SerializeField] GameObject med;
+    [SerializeField] GameObject heavy;
+    [SerializeField] GameObject special;
+    [SerializeField] AudioSource Audio;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,24 +23,66 @@ public class Buttons : MonoBehaviour
     {
        if (Input.GetKey(KeyCode.J))
        {
-            if (mouse.GetComponent<Rigidbody>().linearVelocity.y > 0)
+            if (Vector3.Distance(mouse.transform.position, center.transform.position) > 3)
             {
                 Debug.Log("LightButtonPressed");
+                StartCoroutine(LightAttack());
+                Audio.Play();
             }
             
             
        }
        if (Input.GetKey(KeyCode.K))
        {
-            Debug.Log("MediumButtonPressed");
-       }
+            if (Vector3.Distance(mouse.transform.position, center.transform.position) > 3)
+            {
+                Debug.Log("MediumButtonPressed");
+                StartCoroutine(MedAttack());
+                Audio.Play();
+            }
+        }
        if (Input.GetKey(KeyCode.L))
        {
-            Debug.Log("HeavyButtonPressed");
-       }
+            if (Vector3.Distance(mouse.transform.position, center.transform.position) > 3)
+            {
+                Debug.Log("HeavyButtonPressed");
+                StartCoroutine(HeavyAttack());
+                Audio.Play();
+            }
+        }
         if (Input.GetKey(KeyCode.Comma))
         {
-            Debug.Log("SpecialButtonPressed");
+            if (Vector3.Distance(mouse.transform.position, center.transform.position) > 3)
+            {
+                Debug.Log("SpecialButtonPressed");
+                StartCoroutine(specialAttack());
+                Audio.Play();
+            }
         }
     }
+    IEnumerator LightAttack()
+    {
+        Light.GetComponent<MeshRenderer>().materials[0].color = Color.red;
+        yield return new WaitForSeconds(1);
+        Light.GetComponent<MeshRenderer>().materials[0].color = Color.gray;
+    }
+    IEnumerator MedAttack()
+    {
+        med.GetComponent<MeshRenderer>().materials[0].color = Color.green;
+        yield return new WaitForSeconds(1);
+        med.GetComponent<MeshRenderer>().materials[0].color = Color.gray;
+    }
+    IEnumerator HeavyAttack()
+    {
+        heavy.GetComponent<MeshRenderer>().materials[0].color = Color.blue;
+        yield return new WaitForSeconds(1);
+        heavy.GetComponent<MeshRenderer>().materials[0].color = Color.gray;
+    }
+    IEnumerator specialAttack()
+    {
+        special.GetComponent<MeshRenderer>().materials[0].color = Color.yellow;
+        yield return new WaitForSeconds(1);
+        special.GetComponent<MeshRenderer>().materials[0].color = Color.gray;
+    }
 }
+
--- Assets/MousePosition.cs
+++ Assets/MousePosition.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class MousePosition : MonoBehaviour
 {
     Vector3 mousePos;
+    [SerializeField] float velocityX;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +13,7 @@ public class MousePosition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         mousePos = Input.mousePosition;
         Vector3 worldPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 1));
         gameObject.transform.position = worldPos;
